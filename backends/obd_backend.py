971407cdc1452e380ae842a5a02da60712e6d342
dyphen12 <alexiswong10@gmail.com>
2025-12-17 19:44:33 -0400

    Update obd_backend.py

--- backends/obd_backend.py
+++ backends/obd_backend.py
@@ -76,7 +76,7 @@ class OBDBackend:
             print("🔍 Recuperando RPM...")
             cmd = obd.commands.SPEED # select an OBD command (sensor)
 
-            response = connection.query(cmd) # send the command, and parse the response
+            response = self.connection.query(cmd) # send the command, and parse the response
 
             #print(response.value) # returns unit-bearing values thanks to Pint
             #print(response.value.to("mph")) # user-friendly unit conversions
